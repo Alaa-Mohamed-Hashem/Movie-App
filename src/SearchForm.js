8b8706f5dcdb8eab97ffd815da196ae09df34638
alaa hashem <ahashem6159@gmail.com>
2021-12-20 14:17:45 +0200

    First Commit

---
+++ src/SearchForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+const SearchForm = () => {
+  const { query, setQuery, error } = useGlobalContext();
+
+  return (
+    <form className='search-form' onSubmit={e => e.preventDefault()}>
+      <h2>Search Movies</h2>
+      <input
+        className='form-input'
+        type="text"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+      {error.show && <div className='error'>Movies Not Found !</div>}
+    </form>
+  )
+};
+
+export default SearchForm;
---
+++ src/context.js
@@ -0,0 +1,21 @@
+import React, { useState, useContext } from 'react';
+import useFetch from './useFetch';
+// make sure to use https
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState('batman');
+
+  const { loading, error, data: movies } = useFetch(`&s=${query}`)
+
+  return <AppContext.Provider value={{ loading, error, movies, query, setQuery }}>
+    {children}
+  </AppContext.Provider>
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
